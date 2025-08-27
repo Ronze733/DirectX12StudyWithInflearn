ccfcd597601fa0132e28b93778d14dd5fef4482c
Ronze733 <forttrl20@gmail.com>
2025-08-27 22:41:14 +0900

    first commit.
    base code DX12 lib
    
    with Inflearn by Rookiss

---
+++ Client/Game.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "EnginePch.h"
+
+class Game
+{
+public:
+	void Init(const WindowInfo& info);
+	void Update();
+};
+
---
+++ Engine/EnginePch.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+#include "EnginePch.h"
+#include "Engine.h"
+
+unique_ptr<Engine> GEngine = make_unique<Engine>();
+
+void HelloEngine()
+{
+}
